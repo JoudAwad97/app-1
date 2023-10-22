7aa26f7591acaf1a3d96a3571f58d44bc965e0fc
Joud Awad <joud.awad@metalabdesign.com>
2023-10-23 01:04:28 +0300

    add google routing

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.get("/dummy", (req, res) => {
   res.json({ message: "Dummy response from package 1" });
 });
 
+app.get("/google", async (req, res) => {
+  try {
+    const response = await axios.get("https://google.com");
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to call endpoint" });
+  }
+});
+
 app.get("/call-endpoint", async (req, res) => {
   try {
     const response = await axios.get("http://app-2.retail-store");
