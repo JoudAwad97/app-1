14353ab265132cc0d45b1213b01cff78b2d0fc78
Joud Awad <joud.awad@metalabdesign.com>
2023-10-29 16:00:01 +0300

    change the welcome message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Home page of package 1 blue/green!");
+  res.send("Welcome to the Home page of package 1!");
 });
 
 app.get("/dummy", (req, res) => {
